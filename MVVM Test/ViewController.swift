7d2abde5db462dec350da5fed649bba7666e615c
Amadeu Cavalcante <amadeu01@gmail.com>
2019-02-11 19:42:14 -0200

    Initial Commit

---
+++ MVVM Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM Test
+//
+//  Created by Amadeu Cavalcante Filho on 11/02/19.
+//  Copyright © 2019 Amadeu Cavalcante Filho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
